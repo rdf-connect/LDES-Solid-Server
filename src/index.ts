11333598018a0a639f3bec649d3ac4eb9126c534
ajuvercr <arthur.vercruysse@ugent.be>
2022-03-03 16:44:27 +0100

    correctly route .acl files to the acl handler

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import { Conditions, Patch, Representation, RepresentationPreferences, ResourceIdentifier, ResourceStore } from "@solid/community-server";
+import { Conditions, getLoggerFor, Logger, Patch, Representation, RepresentationPreferences, ResourceIdentifier, ResourceStore } from "@solid/community-server";
 
 
 export class LDESAccessorBasedStore implements ResourceStore {
   private readonly way: string;
+
   constructor(way: string) {
+    
     this.way = way;
   }
 
@@ -17,18 +19,22 @@ export class LDESAccessorBasedStore implements ResourceStore {
   }
 
   setRepresentation = async (identifier: ResourceIdentifier, representation: Representation, conditions?: Conditions | undefined): Promise<ResourceIdentifier[]> => {
+    console.log("Set representation", identifier, representation, conditions)
     throw "Not implemented set"
   }
 
   addResource = async (container: ResourceIdentifier, representation: Representation, conditions?: Conditions | undefined): Promise<ResourceIdentifier> => {
+    console.log("Add representation", container, representation, conditions)
     throw "Not implemented add"
   }
 
   deleteResource = async (identifier: ResourceIdentifier, conditions?: Conditions | undefined): Promise<ResourceIdentifier[]> => {
+    console.log("Delete representation", identifier, conditions)
     throw "Not implemented delete"
   }
 
   modifyResource = async (identifier: ResourceIdentifier, patch: Patch, conditions?: Conditions | undefined): Promise<ResourceIdentifier[]> => {
+    console.log("Modify representation", identifier, patch, conditions)
     throw "Not implemented modify"
   }
 }
